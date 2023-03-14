cfc38ccc0d1ae5aa42abce3397cb1755f0a1322a
Sebastian Gallo Leiva <elgalo13@gmail.com>
2023-03-14 13:10:45 -0400

    Second commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,10 +15,13 @@ urlpatterns = [
     path(route='contact/', view=views.get_contact, name='contact'),
 
     # path for registration
+    path(route='registration/', view=views.registration_request, name='registration'),
 
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
@@ -26,4 +29,4 @@ urlpatterns = [
 
     # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
