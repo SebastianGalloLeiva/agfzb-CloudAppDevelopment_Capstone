21b42273ddd7352ed4390bf88290ad371db0fe85
Sebastian Gallo Leiva <elgalo13@gmail.com>
2023-03-15 16:20:48 -0400

    tercer commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -24,8 +24,10 @@ urlpatterns = [
     path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
-
+    
     # path for dealer reviews view
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    
 
     # path for add a review view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import *
+from .restapis import *
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
 import json
+from django.db import models
+from django.core import serializers
+from django.utils.timezone import now
+import uuid
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -80,14 +84,30 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
+        context = {}
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/3dc499df-22c6-4250-872c-435c1fbc7775/dealership-package/get-dealership"
+        dealerships = get_dealers_from_cf(url)
+        context["dealership_list"] = dealerships
         return render(request, 'djangoapp/index.html', context)
 
-
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        context = {}
+        dealer_url = "https://us-south.functions.appdomain.cloud/api/v1/web/orgg1_djangoserver-space/dealership-package/get-dealership"
+        dealer = get_dealer_by_id_from_cf(dealer_url, dealerId= dealer_id)
+        context["dealer"] = dealer
+    
+        review_url = "https://us-south.functions.appdomain.cloud/api/v1/web/3dc499df-22c6-4250-872c-435c1fbc7775/dealership-package/get-review"
+        reviews = get_dealer_reviews_from_cf(review_url, dealerId= dealer_id)
+        print(reviews)
+        context["reviews"] = reviews
+        
+        return render(request, 'djangoapp/dealer_details.html', context)
+
+
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
