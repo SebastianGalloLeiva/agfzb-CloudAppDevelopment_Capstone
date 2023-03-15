5fe46dbc123db06f68c6343788ad1fd5f4d91014
Sebastian Gallo Leiva <elgalo13@gmail.com>
2023-03-14 20:05:36 -0400

    Third Commit

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
@@ -11,3 +11,22 @@ from django.contrib import admin
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
+
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year')
+    list_filter = ['year']
+    search_fields = ['name', 'type']
+
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+
+# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
